4bc3349092e56da01c13e1be1644ae62dda1cbab
signeamalie <eaa23saj@students.eaaa.dk>
2025-10-22 00:54:30 +0200

    Rette SCSS setup og projekt struktur

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/styles/App.css'
+import './assets/styles/main.scss'
+
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import './index.css'
-
 import './assets/styles/main.scss'
 
 createRoot(document.getElementById('root')!).render(
