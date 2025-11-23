936972f215aa9e9a3d649cf0eb84482ad9016a71
signeamalie <eaa23saj@students.eaaa.dk>
2025-11-23 17:09:42 +0100

    hero.tsx

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -1,3 +1,5 @@
+// hero-sektion med stort billede og tekst overlay
+
 import Image from "../atoms/Image";
 import Heading from "../atoms/Heading";
 import Text from "../atoms/Text";
@@ -21,20 +23,25 @@ export default function Hero({
 }: HeroProps) {
   return (
     <section className={`hero ${className ?? ""}`}>
+      {/* baggrundsbillede */}
       <div className="hero__media">
         <Image src={imageSrc} alt={imageAlt} size="hero" className="hero__image" />
       </div>
 
+      {/* panel ovenpå billedet */}
       <div className="hero__overlay">
         <div className="hero__panel">
+          {/* lille label tekst over overskriften */}
           {kicker && (
             <Text variant="kicker-large" className="hero__kicker">
               {kicker}
             </Text>
           )}
+          {/* hovedoverskrift */}
           <Heading level={1} className="hero__title">
             {title}
           </Heading>
+          {/* brødtekst i panelet */}
           <Text variant="lead" className="hero__body">
             {body}
           </Text>
